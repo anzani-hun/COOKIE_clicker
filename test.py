c59a13722511719c8f864e38674585019b1a8462
anzani-hun <jerikostreet@gmail.com>
2024-05-18 14:24:18 +0200

    frissítve

--- test.py
+++ test.py
@@ -8,27 +8,51 @@ options = webdriver.ChromeOptions()
 options.add_experimental_option("detach", True)
 driver = webdriver.Chrome(options=options)          # options a felette lévő két sor azért kell, hogy a böngészőt nyitva hagyja, ne zárja be
 
-driver.get("https://orteil.dashnet.org/cookieclicker/")
+driver.get('https://orteil.dashnet.org/cookieclicker/')
 
-title = driver.title
-print(title)            # a title kiíratása 
 
-def clickBtn(clickable_btn_xpath):
-    button = WebDriverWait(driver, 10).until(
-        EC.element_to_be_clickable((By.XPATH, clickable_btn_xpath))
-    )
-    button.click()
+def waitFor(xpath=False, id=False):
+    wait = WebDriverWait(driver, 10)
+
+    consentBtn = None
+
+    if xpath:
+        consentBtn = wait.until(
+            EC.presence_of_element_located((By.XPATH, xpath))
+        )
+    elif id:
+        consentBtn = wait.until(
+            EC.presence_of_element_located((By.ID, id))
+        )
+
+    return consentBtn
 
 # első gombra kattint automatán: CONSENT
-clickBtn("/html/body/div[3]/div[2]/div[1]/div[2]/div[2]/button[1]")
+consentBtn = waitFor(xpath="/html/body/div[3]/div[2]/div[1]/div[2]/div[2]/button[1]/p")
+consentBtn.click()
 
 # második gombra kattint automatán: ENGLISH nyelv kiválasztása gomb
-clickBtn("/html/body/div[2]/div[2]/div[12]/div/div[1]/div[1]/div[2]")
+englishLang = waitFor(xpath='//*[@id="langSelect-EN"]')
+englishLang.click()
 
-title = driver.title
-print(title)            # a title kiíratása 
+cookie = waitFor(id='bigCookie')
 
-cookies = WebDriverWait(driver, 10).until(
-    EC.element_to_be_clickable((By.ID, 'cookies'))
-    )
-cookies.click()
+cookieNumber = waitFor(id='cookies')
+
+cookie.click()
+
+def testShop():
+    items = driver.find_elements(By.CLASS_NAME, "unlocked")
+    num = int(cookieNumber.text.split(' ')[0])
+
+    for item in items:
+        price = item.find_element(By.CLASS_NAME, "price")
+
+        if int(price.text.replace(',','')) < num:
+            item.click()
+
+while True:
+    testShop()
+    cookie.click()
+
+input("Press Enter to close the browser...")
